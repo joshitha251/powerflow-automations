2741847aef807d497356d5fe3b405aee84d79508
Joshitha <joshitha1903@gmail.com>
2026-09-22 00:07:45 +0530

    sitemap update

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://powerflow-automations.com",
+      url: "https://www.powerflow-automations.com",
       lastModified: new Date(),
     },
   ];
